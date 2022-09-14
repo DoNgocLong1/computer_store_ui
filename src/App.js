d15bcf8b6be80b5ba488be101b293541cdebceb7
Do Ngoc Long <dongoclong101001@gmail.com>
2022-09-13 21:28:15 -0700

    update offset bar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import {Routes, Route} from 'react-router-dom'
 import './grid.css'
 import Product from './components/Products/Products';
 import BottomBar from './components/BottomBar/BottomBar';
-import {close, show, graphicsCard, computers} from'./components/Products/ProductData.js'
+import {close, show} from'./components/Products/ProductData.js'
+import products from './data/products/products'
 function App() {
   const theme = 'dark_theme'
     document.body.classList.add(theme)
@@ -19,7 +20,6 @@ function App() {
     const [cartBar, setCartBar] = useState(false)
     //set State dataCartBarr để đổ dữ liệu vào cartBar
     const [dataCartBar, setDataCartBar] = useState([])
-    const productList = [...computers, ...graphicsCard]
     useEffect(() => {
       const bottomBar = document.querySelector('.bottom__bar.cart__bar')
       const closeBottomBar = document.querySelector('.bottom__bar.cart__bar .closebar')   
@@ -27,7 +27,7 @@ function App() {
       closeBottomBar.addEventListener('click', () => {
           bottomBar.classList.remove('show')
       })
-        const items = productList.filter((item) => {
+        const items = products.filter((item) => {
             return storageCart.includes(item.id)
         })
         setDataCartBar(items)
@@ -51,35 +51,34 @@ function App() {
       const jsoncart = JSON.stringify(cartItem);
       localStorage.setItem('CART_LIST', jsoncart);
     }
-  console.log(dataCartBar)
-  return (
-    <>
-      <Header/>
-      <div className='container'>
-          <Routes>
-            <Route exact path='/' element = {<Home/>}/>
-            <Route path='/Products' element = {<Product onClick ={handlCartItem}/>}/>
-            {/* <Route path='/Contact' element = {<Contact/>}/> */}
-          </Routes>
-          <div id='cart_btn' onClick={show}>
-            <div className='cart__btn__item'>
-              <span>{dataCartBar.length}</span>
-              <div className='cart__btn__title'>
-                <span > Xem giỏ hàng</span>
-              </div>          
-              <i className="fas fa-shopping-cart"></i>
-            </div>     
-          </div>    
-      </div>
-      <Footer/>
-      <BottomBar
-        data = {dataCartBar}
-        className = 'cart__bar'
-        title = 'Giỏ hàng của bạn'
-        onClick = {close}
-      />
-    </>
-  );
+    return (
+      <>
+        <Header/>
+        <div className='container'>
+            <Routes>
+              <Route exact path='/' element = {<Home/>}/>
+              <Route path='/Products' element = {<Product onClick ={handlCartItem}/>}/>
+              {/* <Route path='/Contact' element = {<Contact/>}/> */}
+            </Routes>
+            <div id='cart_btn' onClick={show}>
+              <div className='cart__btn__item'>
+                <span>{dataCartBar.length}</span>
+                <div className='cart__btn__title'>
+                  <span > Xem giỏ hàng</span>
+                </div>          
+                <i className="fas fa-shopping-cart"></i>
+              </div>     
+            </div>    
+        </div>
+        <Footer/>
+        <BottomBar
+          data = {dataCartBar}
+          className = 'cart__bar'
+          title = 'Giỏ hàng của bạn'
+          onClick = {close}
+        />
+      </>
+    );
 }
 
 export default App;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,8 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect, useLayoutEffect} from "react";
 import './Products.css'
 import ProductList from'../ProductList/ProductList'
 import BottomBar from "../BottomBar/BottomBar";
-import '../../data/products/graphicsCard'
-import '../../data/products/computers'
-import {graphicsCard, computers} from './ProductData'
+import products from '../../data/products/products'
 function Products({onClick}) {
     //--------phần lấy dữ liệu và set state cho danh mục yêu thích --------- 
     //lấy dữ liệu các sản phẩm ưu thích từ LocalStorage nếu chưa có FAVOURITE_LIST trong LocalStorage thì khởi tạo FAVOURITE_LIST rỗng
@@ -15,7 +13,8 @@ function Products({onClick}) {
     const [favouriteBar, setFavouriteBar] = useState(false)
     //set State dataFavouriteBar để đổ dữ liệu vào favouriteBar
     const [dataFavouriteBar, setDataFavouriteBar] = useState([])
-    const productList = [...computers, ...graphicsCard]
+    const [softRule, setSoftRule] = useState('default')
+    const [softItem, setSoftItem] = useState('all')
     useEffect(() => {
         const bottomBar = document.querySelector('.bottom__bar.favourite__bar')
         const closeBottomBar = document.querySelector('.bottom__bar.favourite__bar .closebar')   
@@ -23,7 +22,7 @@ function Products({onClick}) {
         closeBottomBar.addEventListener('click', () => {
             bottomBar.classList.remove('show')
         })
-        const items = productList.filter((item) => {
+        const items = products.filter((item) => {
             return storageFavourite.includes(item.id)
         })
         setDataFavouriteBar(items)
@@ -33,6 +32,67 @@ function Products({onClick}) {
             })
         }
     },[favouriteList])
+
+    //xử lý sắp xếp sản phẩm
+    let data;
+    const handleIncreseSoft = () => {
+        products.sort((prevItem, nextItem) => {
+        return prevItem.price.split('.').join('') - nextItem.price.split('.').join('')
+        })
+        data = products
+    }
+    const handleDecreseSoft = () => {
+        products.sort((prevItem, nextItem) => {
+        return nextItem.price.split('.').join('') - prevItem.price.split('.').join('')
+        })
+        data = products
+    }
+    switch(softRule) {
+        case 'increase' :
+            handleIncreseSoft()
+            break;
+        case 'decrease' :
+            handleDecreseSoft()
+            break;
+        default:
+            data = products
+            break;
+    }
+
+
+    //xử lý lọc sản phẩm
+    const tabs = document.querySelectorAll('.select_items li')   
+        tabs.forEach((tab) => {
+            tab.addEventListener('click', (e) => {  
+                const tabActive = document.querySelector('.select_items li.active')             
+                tabActive.classList.remove('active')               
+                e.target.classList.add('active')
+                setSoftItem( e.target.getAttribute('value'))
+            })
+        })
+    switch(softItem) {
+        case 'laptop' :
+            data = products.filter((item) => {
+                return item.type === 'laptop'
+            })
+            break;
+        case 'monitor' :
+            data = products.filter((item) => {
+                return item.type === 'monitor'
+            })
+            break;
+        case 'graphiccard' :
+            data = products.filter((item) => {
+                return item.type === 'graphicCard'
+            })
+            break;
+        case 'all' :
+            data = products
+            break;
+        default:
+            data = products
+            break;
+    }  
     //hàm xử lý thêm và bỏ yêu thích
     const handleFavouriteItem = (name) => {
         setFavouriteBar(true)
@@ -48,20 +108,49 @@ function Products({onClick}) {
         const jsonFavourite = JSON.stringify(favouriteItem)
         localStorage.setItem('FAVOURITE_LIST', jsonFavourite)
     }
+    let offsetBarLength = Math.ceil(data.length / 15) 
+    let rows = []
+    for( let i = 0; i<offsetBarLength; i++ ) {
+        rows.push(i)
+    }
+    const [startItems, setStartItems] = useState(0)
+    const [lastItems, setLastItems] = useState(15)
+    const [currentOffsetItems, setCurrentOffsetItems] = useState([])
+    useLayoutEffect(() => {
+        const offsetitem  = data.slice(startItems, lastItems)
+        setCurrentOffsetItems(offsetitem)
+    }, [startItems])
+    const handleNextBtn = () => {
+        if(lastItems < data.length){
+            setStartItems(prev => prev + 15)
+            setLastItems(prev => prev + 15)
+        } 
+    }
+    const changeOffsetItemByNumber = (e) =>{
+        const offset = +(e.target.value) * 15
+        setStartItems(offset)
+        setLastItems(offset + 15)
+    }
+    const handlePrevBtn = () => {
+        if(startItems > 0){
+            setStartItems(prev => prev - 15)
+            setLastItems(prev => prev - 15)
+        }      
+    }
     return (
         <div id="Products">
             <div className='product__container grid wide'>
                 <div className="row">
                     <nav className="select_items">
                         <ul>
-                            <li className="active">Tất cả</li>
-                            <li>laptop</li>
-                            <li>Màn hình</li>
-                            <li>Card đồ họa</li>
+                            <li className="active" value = "all">Tất cả</li>
+                            <li value = "laptop">laptop</li>
+                            <li value = "monitor">Màn hình</li>
+                            <li value = "graphiccard">Card đồ họa</li>
                         </ul>
-                        <label for="sort" class="form-label">Sắp xếp theo</label>
-                        <select id="sort" name="sort">
-                            <option value="">--Mặc định--</option>
+                        <label htmlFor="sort" className="form-label">Sắp xếp theo</label>
+                        <select id="sort" name="sort" onChange={(e) => {setSoftRule(e.target.value)}}>
+                            <option value="default">--Mặc định--</option>
                             <option value="increase">Giá tăng dần</option>
                             <option value="decrease">Giá giảm dần</option>
                         </select>
@@ -69,16 +158,26 @@ function Products({onClick}) {
                     <ProductList 
                     onClickFavourite = {handleFavouriteItem}
                     onClickCart = {onClick}
-                    data = {graphicsCard}
+                    data = {currentOffsetItems}
                     />
-                    <ProductList 
-                    onClickFavourite = {handleFavouriteItem}
-                    onClickCart = {onClick}
-                    data = {computers}
-                    />
+                    <div className="offset__bar">
+                        <div className="prevbtn btn" onClick={handlePrevBtn}> prev</div>
+                        <div className="offset__cont">                           
+                            {rows.map((number, index) => ( 
+                            <button 
+                            className = "btn" 
+                            value = {number} 
+                            onClick = {changeOffsetItemByNumber}
+                            key = {index}
+                            >
+                                {number + 1}
+                            </button>))                            
+                            }
+                        </div>
+                        <div className="nextbtn btn" onClick = {handleNextBtn}> next</div>
+                    </div>
                 </div>
-            </div> {
-            }
+            </div>
             <BottomBar 
             data = {dataFavouriteBar}
             className = 'favourite__bar'
